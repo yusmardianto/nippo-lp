27a4c9af2de6d3178e170561fbe593ce3002014a
Gontang Prakasa <gontang@logique.co.id>
2020-07-09 22:50:17 +0700

    add validation and link url

--- ina/contact.php
+++ ina/contact.php
@@ -27,6 +27,7 @@
 	
 	<!-- Font Awesome -->
 	<link rel="stylesheet" href="/assets/fonts/font-awesome/css/font-awesome.min.css">
+    <!-- <link rel="stylesheet" href="https://jqueryvalidation.org/files/demo/site-demos.css"> -->
     
 	<!-- Custom Style -->
 	<link href="/assets/css/style.css" rel="stylesheet">
@@ -61,27 +62,27 @@
         <div class="container">
             <div class="row">
                 <div class="offset-lg-2 offset-md-1 col-lg-8 col-md-10">
-                    <form action="https://formspree.io/xeqrrryp" method="POST">
+                    <form action="https://formspree.io/gontang1500018280@webmail.uad.ac.id" method="POST" id="registration">
                         <div class="row">
                             <div class="col-md-6">
                                 <label for="contact_name">Nama <span class="text-danger">*</span></label>
-                                <input class="form-control" type="text" id="contact_name" required>
+                                <input class="form-control" type="text" id="contact_name" name="contact_name" >
                             </div>
                             <div class="col-md-6">
                                 <label for="contact_company">Perusahaan <span class="text-danger">*</span></label>
-                                <input class="form-control" type="text" id="contact_company" required>
+                                <input class="form-control" type="text" id="contact_company" name="contact_company" >
                             </div>
                             <div class="col-md-6">
                                 <label for="contact_phone">Nomor Telp. <span class="text-danger">*</span></label>
-                                <input class="form-control" type="tel" id="contact_phone" required>
+                                <input class="form-control" type="tel" id="contact_phone" name="contact_phone" >
                             </div>
                             <div class="col-md-6">
                                 <label for="contact_email">Email <span class="text-danger">*</span></label>
-                                <input class="form-control" type="email" id="contact_email" required>
+                                <input class="form-control" type="email" id="contact_email" name="contact_email" >
                             </div>
                             <div class="col-md-12">
                                 <label for="contact_message">Pesan <span class="text-danger">*</span></label>
-                                <textarea class="form-control" name="contact_message" id="contact_message" rows="4" required></textarea>
+                                <textarea class="form-control" name="contact_message" id="contact_message" rows="4" ></textarea>
                             </div>
                             <div class="col text-center text-md-left">
                                 <button class="btn btn--green btn--round btn--main" type="submit">Kirim &nbsp; <img class="arrow-right arrow-right--white" src="/assets/icon/arrow-right.svg" alt="" width="15"></button>
@@ -113,6 +114,8 @@
 	<script src="/assets/js/slick.min.js"></script>		
     <script src="/assets/js/jquery.matchHeight-min.js"></script>
     <script src="/assets/js/main.js"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/jquery-validation@1.19.1/dist/jquery.validate.min.js"></script>
 	<script type="application/ld+json">
 	{
 		"@context": "https://schema.org",
@@ -128,5 +131,41 @@
 		"telephone": "XXXXXX"
 	}
 </script>
+<script>    
+    $(function() {
+        $("form#registration").validate({
+            rules: {
+            contact_name: "required",
+            contact_company: "required",
+            contact_email: {
+                required: true,
+                email: true
+            },
+            contact_phone: {
+                required: true,
+                number: true
+            },
+            contact_message: "required"
+            },
+            // Specify validation error messages
+            messages: {
+                contact_name: "Please enter your contact name",
+                contact_company: "Please enter your contact company",
+                contact_email: {
+                    required : "Please enter your email address",
+                    email: "Please enter a valid email address"
+                },
+                contact_phone: {
+                    required: "Please enter your contact_name",
+                    number: "Please enter your weight as a numerical value"
+                },
+                contact_message: "Please enter your contact message",
+            },
+            submitHandler: function(form) {
+            form.submit();
+            }
+        });
+    });
+    </script>
   </body>
 </html>
