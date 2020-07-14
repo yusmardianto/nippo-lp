4145e1efdb773ab21a797065eed2db123dfb7329
Bagas Ranggen <bagas@logique.co.id>
2020-07-14 11:58:18 +0700

    add google tag manager add google search console

--- ina/404.php
+++ ina/404.php
@@ -29,6 +29,10 @@
   <?php $page = '404.php'; ?>
 
   <body>
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KBWKGBV"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
 	  
 	<?php include './layout/header.php' ?>
 	  
--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <meta name="description" itemprop="description" content="">
 	<meta name="keywords" content="">
 	<meta name="author" content="">
-    <meta name="google-site-verification" content="XXXXXXXXXXXXXXXXXXXXXXXXXXXX" />
+    <meta name="google-site-verification" content="vA3R6dl4jXfUVzsjkNcL_3BRHXnpoR1v6PZH_OMi4AY" />
 	<meta itemprop="name" content="">
     <meta property="og:title" content="">
     <meta property="og:type" content="website">
@@ -15,6 +15,7 @@
     <meta property="og:url" content="">
     <meta property="og:image" content="">
     <meta property="og:description" content="">
+    <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
 	
     <title>Nippo LP</title>
 
@@ -33,20 +34,38 @@
 	<link href="/assets/css/header-footer.css" rel="stylesheet">
 	<link href="/assets/css/home.css" rel="stylesheet">
 	
-	<!-- Global site tag (gtag.js) - Google Analytics -->
-	<!-- <script async src="https://www.googletagmanager.com/gtag/js?id=UA-XXXXXXXXX-X"></script>
-	<script>
-	  window.dataLayer = window.dataLayer || [];
-	  function gtag(){dataLayer.push(arguments);}
-	  gtag('js', new Date());
-	  gtag('config', 'UA-XXXXXXXXX-X');
-	</script> -->
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-172657169-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-172657169-1');
+    </script>
+    <!-- End Global site tag (gtag.js) - Google Analytics -->
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-KBWKGBV');</script>
+    <!-- End Google Tag Manager -->
+
 
   </head>
   
   <?php $page = ''; ?>
 
   <body>
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KBWKGBV"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KBWKGBV"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
     
     <?php include './layout/header.php' ?>
 
